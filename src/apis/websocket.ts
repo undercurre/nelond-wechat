1448ab81b120985516f3945e5a65f5771fbac632
KEVIN YANG <yangws2@midea.com>
2023-12-21 09:53:57 +0800

    feat: 完善ws信息机制

--- src/apis/websocket.ts
+++ src/apis/websocket.ts
@@ -1,15 +1,16 @@
 import { mzaioWSURL, getEnv } from '../config/index'
 import { projectStore } from '../store/index'
-import { Logger, storage } from '../utils/index'
+import { Logger, storage, strUtil } from '../utils/index'
 
 /**
  * 建立某个空间的webSocket连接
  * @param projectId 项目id
  */
-export function connectHouseSocket(projectId: string) {
+export function connectSocket(projectId: string) {
   Logger.log('连接项目socket: ', projectId, projectStore.currentProjectDetail.projectName)
+  const url = strUtil.getUrlWithParams(mzaioWSURL[getEnv()], { projectId })
   return wx.connectSocket({
-    url: mzaioWSURL[getEnv()] + projectId,
+    url,
     protocols: [storage.get<string>('token') as string],
     success(res) {
       Logger.log('connectSocket-success', res)
