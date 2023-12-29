299f5ae181fffb68e01dd2242feb272f231c113a
夏钰明 <xiaym18@midea.com>
2023-12-29 16:44:16 +0800

    feat: 由空间内跳转场景时使用新页面而非tabbar页

--- src/package-space-control/index/index.ts
+++ src/package-space-control/index/index.ts
@@ -952,8 +952,11 @@ ComponentWithComputed({
     },
 
     handleSceneTap() {
-      wx.switchTab({
-        url: '/pages/automation/index',
+      // wx.switchTab({
+      //   url: '/pages/automation/index',
+      // })
+      wx.navigateTo({
+        url: '/package-space-control/scene-list/index',
       })
     },
     /** 点击创建场景按钮回调 */
