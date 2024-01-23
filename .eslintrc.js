222b206641a4f83ff6b5cfa662ee1cbf4827f917
KEVIN YANG <yangws2@midea.com>
2024-01-23 17:32:50 +0800

    chore: 代码检查规则调整

--- .eslintrc.js
+++ .eslintrc.js
@@ -41,6 +41,7 @@ module.exports = {
         '@typescript-eslint/no-empty-function': 'off',
         '@typescript-eslint/no-empty-interface': 'off',
         '@typescript-eslint/no-namespace': 'off',
+        '@typescript-eslint/no-non-null-assertion': 'off',
       },
     },
   ],
