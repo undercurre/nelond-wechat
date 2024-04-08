61c5beb717318cbb417a38ca63885a3c85d2869d
KEVIN YANG <yangws2@midea.com>
2024-04-08 17:15:19 +0800

    chore: 修改注释

--- src/utils/validate.ts
+++ src/utils/validate.ts
@@ -7,8 +7,9 @@ export function checkInputNameIllegal(input: string) {
 
 /**
  * 校验输入的用户名是否非法
+ * 最多20个字符
  * 首字符必须是中文、英文字母、括号、圆括号、横杠或下划线中的任意一个；
- * 后续字符可以是中文、英文字母、括号、圆括号、横杠或下划线中的任意一个，也可以是数字0-9、横杠或下划线。
+ * 后续字符可以是数字、中文、英文字母、括号、圆括号、横杠或下划线中的任意一个
  */
 export function checkUserNameIllegal(input: string) {
   return /^[\u4e00-\u9fa5a-zA-Z（）()-_]{1}[\u4e00-\u9fa5a-zA-Z（）()0-9-_]{0,19}$/g.test(input)
