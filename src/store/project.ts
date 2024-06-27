58bab02692e728284ad3fcf82dbe16b46aea55ed
KEVIN YANG <yangws2@midea.com>
2024-06-27 11:09:31 +0800

    style: 增加初始化时的Loading，屏蔽数据不完整时的用户操作

--- src/store/project.ts
+++ src/store/project.ts
@@ -6,7 +6,7 @@ import {
   saveOrUpdateUserHouseInfo,
   queryLocalKey,
 } from '../apis/index'
-import { asyncStorage, storage, IApiRequestOption } from '../utils/index'
+import { asyncStorage, storage, IApiRequestOption, showLoading, hideLoading } from '../utils/index'
 import { deviceStore } from './device'
 import { othersStore } from './others'
 import { spaceStore } from './space'
@@ -61,6 +61,7 @@ export const projectStore = observable({
    * 首页加载逻辑
    */
   async spaceInit() {
+    showLoading()
     const success = this.loadSpaceDataFromStorage()
     if (success) {
       othersStore.setIsInit(true)
@@ -95,6 +96,8 @@ export const projectStore = observable({
       othersStore.setIsInit(true)
       console.log('[KS]云端数据加载成功, isInit:', othersStore.isInit)
     }
+
+    hideLoading()
   },
 
   /**
