b803488021320f348ff07d7a8d4f759c6625c5d6
KEVIN YANG <yangws2@midea.com>
2024-07-04 15:18:24 +0800

    perf: 兼容性补充提交

--- src/store/space.ts
+++ src/store/space.ts
@@ -101,7 +101,7 @@ export const spaceStore = observable({
     const parentSpace = spaceStore.allSpaceList.find((item) => item.spaceId === space?.pid) as Space.allSpace
 
     if (!parentSpace) {
-      return space.spaceName
+      return space?.spaceName ?? ''
     }
 
     return `${parentSpace.pid === '0' ? parentSpace.spaceName : this.getSpaceFullName(parentSpace)},${space.spaceName}`
